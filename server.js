10ef6b18405689db6ae2c42a62698ac9cf266f36
Kyle Jeffrey <kajeffre@ucsc.edu>
2020-07-15 19:22:41 -0700

    updated port:

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const socket = require('socket.io');
 const app = express();
 
 // Set static Folder that will automatically respond to get requests
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 // Listen on port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
 });
