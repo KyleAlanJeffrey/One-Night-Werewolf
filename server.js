5aac341edd2630a1614896aa8a03da5b73501de3
Kyle Jeffrey <kajeffre@ucsc.edu>
2020-07-15 19:37:54 -0700

    dont even kknow

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const socket = require('socket.io');
-
 const app = express();
+const s = require('http').Server(app);
 
 // Set static Folder that will automatically respond to get requests
 app.use(express.static('public'));
 
 // Listen on port
 const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
+s.listen(PORT, () => {
     console.log('listening on port ' + PORT);
 });
 
-const io = socket(server);
+const io = socket(s);
 
 /*-----------------------
         SOCKET IO 
